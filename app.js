44162e7167305b27664ed6e02a3781513316be65
Sander Pick <sanderpick@gmail.com>
2011-05-16 12:24:46 -0700

    for debug
    Change-Id: I799309a22a824db3cdc19e44e4b47d278e7724ac

--- app.js
+++ app.js
@@ -680,7 +680,7 @@ app.put('/cycle', function (req, res) {
   User.findOne({ email: 'jit@ridemission.com' }, function (err, usr) {
   //User.findOne({ email: cycle.userId }, function (err, usr) {
     if (usr) {
-      if (usr.authenticate(req.body.password)) {
+      //if (usr.authenticate(req.body.password)) {
         findVehicle(1299806346, function (veh) {
         //findVehicle(cycle.vehicleId, function (veh) {
           if (veh) {
@@ -689,9 +689,9 @@ app.put('/cycle', function (req, res) {
             res.send({ status: 'fail', data: { code: 'VEHICLE_NOT_FOUND' } });
           }
         });
-      } else {
-        res.send({ status: 'fail', data: { code: 'INCORRECT_PASSWORD' } });
-      }
+      //} else {
+        //res.send({ status: 'fail', data: { code: 'INCORRECT_PASSWORD' } });
+      //}
     } else {
       res.send({ status: 'fail', data: { code: 'USER_NOT_FOUND' } });
     }
