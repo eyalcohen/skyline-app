88410c8cf5a108270937b8841917ee53e2c18360
Seth LaForge <seth@ridemission.com>
2011-12-20 11:55:10 -0800

    Remove stupid debugging test!

--- public/javascripts/sample-cache.js
+++ public/javascripts/sample-cache.js
@@ -319,7 +319,6 @@ define(function () {
       minDuration: req.dur, getMinMax: true,
     };
     App.api.fetchSamples(req.veh, req.chan, options, function(err, samples) {
-      samples();
       if (err) {
         console.error(
             'SampleCache server call fetchSamples(' + req.veh + ', "' +
