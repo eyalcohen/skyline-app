ffa886b7fdf627e3802ec920499c70a07087e261
Eyal Cohen <eyalcohen1@gmail.com>
2014-06-26 18:14:14 -0700

    overview updates

--- public/js/views/overview.js
+++ public/js/views/overview.js
@@ -56,6 +56,23 @@ define([
 
     // Bind mouse events.
     events: {
+      'mouseenter': function(e) {
+        this.cursor.fadeIn('slow');
+      },
+      'mousemove': _.debounce(function(e) {
+        this.cursor.css({left: e.pageX})
+        var time = this.getVisibleTime();
+        var pixLeft = e.pageX;
+        var width = this.$el.width();
+        var d = util.toLocaleString(
+          new Date(((time.end - time.beg) * pixLeft/width + time.beg) / 1e3)
+          , 'mmm d yyyy');
+        this.cursorDate.text(d);
+      }, 5),
+      'mouseleave': function(e) {
+        this.cursor.hide();
+      },
+      'mousedown': 'overviewZoom'
     },
 
     // Misc. setup.
@@ -65,9 +82,9 @@ define([
       this.selection = this.$('.overview-selection');
       this.cursor = this.$('.overview-cursor');
       this.cursorDate = $('.overview-cursor-date');
+      this.cursorDate.mousedown(_.bind(this.overviewZoom, this));
 
       this.visTimePlot = $('<div class="overview-vis">').appendTo(this.$el);
-      this.visTimePlot.bind('mousedown', _.bind(this.overviewZoom, this));
 
       var path = d3.svg.area()
         .x(this.$el.width()/2)
@@ -89,27 +106,6 @@ define([
       _.delay(_.bind(function () {
         this.$el.css({opacity: 1});
       }, this), 500);
-
-      this.$el.mouseenter(_.bind(function(e) {
-        console.log('here');
-        this.cursor.fadeIn();
-      }, this));
-      this.$el.mousemove(_.bind(function(e) {
-        this.cursor.css({left: e.pageX})
-        var time = this.getVisibleTime();
-        var pixLeft = e.pageX - this.$el.offset().left;
-        var width = this.$el.width();
-        var d = util.toLocaleString(
-          new Date(((time.end - time.beg) * pixLeft/width + time.beg) / 1e3), 'mmm d yyyy');
-        //var d = util.toLocaleString(
-        //  new Date((time.end - time.beg)/1e3), 'mmm d yyyy');
-        console.log(pixLeft, width, time, d, time.end - time.beg, (time.end - time.beg) * pixLeft/width);
-        //this.cursorDate.text(e.pageX + ' ' + this.$el.width() + ' ' + this.$el.offset());
-        this.cursorDate.text(d);
-      }, this));
-      this.$el.mouseleave(_.bind(function(e) {
-        this.cursor.fadeOut();
-      }, this));
 
       return this;
     },
@@ -330,6 +326,7 @@ define([
     },
 
     overviewZoom: function(e) {
+      console.log(e);
       var time = this.getVisibleTime();
 
       // Map x-coordinate to time.
